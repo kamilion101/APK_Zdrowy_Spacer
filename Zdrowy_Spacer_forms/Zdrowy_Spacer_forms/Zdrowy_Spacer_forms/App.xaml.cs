f181bcebb376dbb92ebf4674033e5486a3d9156b
kamilion101 <56162797+kamilion101@users.noreply.github.com>
2021-11-12 11:39:39 +0100

    forms

---
+++ Zdrowy_Spacer_forms/Zdrowy_Spacer_forms/Zdrowy_Spacer_forms/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Zdrowy_Spacer_forms.Services;
+using Zdrowy_Spacer_forms.Views;
+
+namespace Zdrowy_Spacer_forms
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Zdrowy_Spacer_forms/Zdrowy_Spacer_forms/Zdrowy_Spacer_forms/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Zdrowy_Spacer_forms.Models;
+
+namespace Zdrowy_Spacer_forms.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
