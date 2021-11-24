80acb0b890623fb88d23e6555ee3dfefd59dd359
Nikolasksk <94048314+Nikolasksk@users.noreply.github.com>
2021-11-24 17:37:52 +0100

    Update MainActivity.cs

--- ZdrowySpacer/ZdrowySpacer/ZdrowySpacer.Android/MainActivity.cs
+++ ZdrowySpacer/ZdrowySpacer/ZdrowySpacer.Android/MainActivity.cs
@@ -33,7 +33,7 @@ namespace ZdrowySpacer.Droid
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
-            {
+            
 
             if (requestCode == RequestLocationID)
                 {
@@ -46,10 +46,7 @@ namespace ZdrowySpacer.Droid
                         //Permissions denied - display a message.
                     }
                 }
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
+         
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
