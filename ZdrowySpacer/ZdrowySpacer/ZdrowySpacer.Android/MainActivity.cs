ea6c531564bd7b222a7ec3978e3b2691fc2833e2
Nikolasksk <94048314+Nikolasksk@users.noreply.github.com>
2021-12-01 18:15:06 +0100

    Dodanie Strony Logowania/ Ikona aplikacji/ oraz znacznika na mapie

--- ZdrowySpacer/ZdrowySpacer/ZdrowySpacer.Android/MainActivity.cs
+++ ZdrowySpacer/ZdrowySpacer/ZdrowySpacer.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Plugin.Media;
 
 namespace ZdrowySpacer.Droid
 {
-    [Activity(Label = "ZdrowySpacer", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "ZdrowySpacer", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         const int RequestLocationID = 0;
