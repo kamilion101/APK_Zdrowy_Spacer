baa8bec2e6cdcdfdba1539eb10b039d982cc5100
kamilion101 <kamilinho101@gmail.com>
2021-12-15 14:06:06 +0100

    current_location_working

---
+++ ZdrowySpacer_v2/ZdrowySpacer/ZdrowySpacer/Mapa.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+
+namespace ZdrowySpacer
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class Mapa : ContentPage
+    {
+        public Mapa()
+        {
+            InitializeComponent();
+            DisplayCurLoc();
+        }
+
+        public async void DisplayCurLoc()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+                var location = await Geolocation.GetLocationAsync(request);
+                if (location != null)
+                {
+                    Position p = new Position(location.Latitude, location.Longitude);
+                    MapSpan mapSpan = MapSpan.FromCenterAndRadius(p, Distance.FromKilometers(.444));
+                    myMap.MoveToRegion(mapSpan);
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+        }
+    }
+}
---
+++ ZdrowySpacer_v2/ZdrowySpacer/ZdrowySpacer/Strona_glowna.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ZdrowySpacer
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Strona_glowna : ContentPage
+    {
+        public Strona_glowna()
+        {
+            InitializeComponent();
+        }
+
+        private async void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            await Shell .Current.GoToAsync($"//{nameof(LoginPage)}");
+        }
+    }
+}
