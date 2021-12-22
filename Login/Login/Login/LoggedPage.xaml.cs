ae99521f98116be43a8a4d2ca91360dafa58578d
kamilion101 <kamilinho101@gmail.com>
2021-12-22 14:00:48 +0100

    Firebase_Login

---
+++ Login/Login/Login/LoggedPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Login
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoggedPage : ContentPage
+    {
+        IAuth auth;
+        public LoggedPage()
+        {
+            InitializeComponent();
+            auth = DependencyService.Get<IAuth>();
+        }
+
+        void SignOutButton_Clicked(object sender, EventArgs e)
+        {
+            var signOut = auth.SignOut();
+
+            if (signOut)
+            {
+                Application.Current.MainPage = new MainPage();
+            }    
+        }
+
+    }
+}
---
+++ Login/Login/Login/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Login
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        IAuth auth;
+        public MainPage()
+        {
+            InitializeComponent();
+            auth = DependencyService.Get<IAuth>();
+        }
+
+        async void LoginClicked(object sender, EventArgs e)
+        {
+            string token = await auth.LoginWithEmailAndPassword(EmailInput.Text, PasswordInput.Text);
+
+            if (token != string.Empty)
+            {
+                Application.Current.MainPage = new LoggedPage();
+            }
+            else
+            {
+                await DisplayAlert("Authentication Failed", "Email or Password are incorrect", "Ok");
+            }
+        }
+
+        void SignUpClicked(object sender, EventArgs e)
+        {
+            var signOut = auth.SignOut();
+
+            if (signOut)
+            {
+                Application.Current.MainPage = new SignUpPage();
+            }
+        }
+
+    }
+}
